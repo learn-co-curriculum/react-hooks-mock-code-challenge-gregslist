741948344bebf004f16f21eecc208c2e68bba739
ihollander <ianholla@gmail.com>
2021-01-15 15:56:36 -0500

    Fixed card

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -4,7 +4,7 @@ function ListingCard() {
   return (
     <li className="card">
       <div className="image">
-        <span className="price">Free</span>
+        <span className="price">$0</span>
         <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
       </div>
       <div className="details">
