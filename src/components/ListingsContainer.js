3f304e25eae9fd8211157f1d59e6d47f759e3d26
Juan Velasco <juanayevela@yahoo.com>
2022-05-04 11:56:22 -0400

    commit tree 202c1409c8e2994a8918083e5ed4e86a380a8f98

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,11 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        <ListingCard />
       </ul>
     </main>
   );
